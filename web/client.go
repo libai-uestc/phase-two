64099d2d46c78407b28199a32e1a87de8d0d4ea4
libai <18101611430@163.com>
2026-09-24 23:53:35 +0800

    新增测试文件和client.go相关函数

--- web/client.go
+++ web/client.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
+	"libai/go/phase-two/web/idl"
 	"log/slog"
 	"net/http"
+	"net/url"
 	"os"
+
+	"go.yaml.in/yaml/v3"
+	"google.golang.org/protobuf/proto"
 )
 
 // go run ./web
@@ -56,6 +63,64 @@ func Get(path string) {
 	}
 	processResponse(resp)
 }
+
+func GetStudentPB(path string) {
+	fmt.Println("GET " + path)
+	resp, err := http.Get("http://127.0.0.1:5678" + path)
+	if err != nil {
+		slog.Error("http get failed", "error", err)
+		return
+	}
+	defer resp.Body.Close()
+	fmt.Println("响应头：")
+	for k, v := range resp.Header {
+		fmt.Printf("%s=%s\n", k, v[0])
+	}
+	if resp.StatusCode != http.StatusOK {
+		slog.Error("异常状态码", "http response code", resp.StatusCode)
+		io.Copy(os.Stdout, resp.Body)
+	} else {
+		bs, _ := io.ReadAll(resp.Body)
+		var stu idl.Student
+		if err := proto.Unmarshal(bs, &stu); err == nil {
+			fmt.Printf("pb反序列化成功, name=%s,addr=%s\n", stu.Name, stu.Address)
+		}
+	}
+	os.Stdout.WriteString("\n\n")
+}
+
+func PostForm(path string,stu Student){
+	fmt.Println("post form " + path)
+	if resp,err := http.PostForm("http://127.0.0.1:5678"+path,url.Values
+	{"username":{stu.Name},"addr":{stu.Address}})
+}
+
+func PostJson(path string,stu Student){
+	fmt.Println("post json "+ path)
+	if bs,err := json.Marshal(stu);err == nil {
+		if resp,err := http.Post("http://127.0.0.1:5678"+path,"application/xml",bytes.NewReader(bs));err != nil{
+			panic(err)
+		} else {
+			processResponse(resp)
+		}
+	} else {
+		slog.Error("xml marchal failed","error",err)
+	}
+}
+
+func PostYaml(path string,stu Student){
+	fmt.Println("post yaml " + path)
+	if bs,err := yaml.Marshal(stu);err == nil {
+		if resp,err := http.Post("http://127.0.0.1:5678"+path, "application/json",bytes.NewReader(bs)); err != nil {
+			panic(err)
+		} else {
+			processResponse(resp)
+		}
+	} else {
+		slog.Error("json marchal failed","error",err)
+	}
+}
+
 
 func main() {
 	Get("/home")
--- web/gin/request.go
+++ web/gin/request.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"libai/go/phase-two/web/idl"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
 func url(engine *gin.Engine) {
@@ -20,4 +26,181 @@ func restful(engine *gin.Engine) {
 		addr := ctx.Param("addr")
 		ctx.String(http.StatusOK, name+" live in "+addr)
 	})
+}
+
+func postForm(engine *gin.Engine) {
+	engine.POST("/student/form", func(ctx *gin.Context) {
+		name := ctx.PostForm("username")
+		addr := ctx.DefaultPostForm("addr", "China")
+		ctx.String(http.StatusOK, name+" live in "+addr)
+	})
+}
+
+func postJson(engine *gin.Engine) {
+	engine.POST("/student/json", func(ctx *gin.Context) {
+		var stu Student
+		bs, _ := io.ReadAll(ctx.Request.Body)
+		if err := json.Unmarshal(bs, &stu); err == nil {
+			name := stu.Name
+			addr := stu.Addr
+			ctx.String(http.StatusOK, name+" live in "+addr)
+		}
+	})
+}
+
+func ShouldBindBodyWith(ctx *gin.Context) error {
+	var body []byte
+	if v, exists := ctx.Get("rbody"); exists {
+		if bs, ok := v.([]byte); ok {
+			body = bs
+		}
+	}
+	if body == nil {
+		body, _ = io.ReadAll(ctx.Request.Body)
+		ctx.Set("rbody", body)
+	}
+	var stu Student
+	return json.Unmarshal(body, &stu)
+}
+
+func Handler(ctx *gin.Context) {
+	var stu Student
+	ctx.BindJSON(&stu)
+	ctx.String(200, stu.Name+stu.Addr)
+}
+
+func upload_file(engine *gin.Engine) {
+	engine.MaxMultipartMemory = 8 << 20
+	engine.POST("/upload", func(ctx *gin.Context) {
+		file, err := ctx.FormFile("file")
+		if err != nil {
+			fmt.Printf("get file error %v\n", err)
+			ctx.String(http.StatusInternalServerError, "upload file failed")
+		} else {
+			if err = ctx.SaveUploadedFile(file, "./data/"+file.Filename); err == nil {
+				ctx.String(http.StatusOK, file.Filename)
+			} else {
+				fmt.Printf("save file to %s failed: %v\n", "./data/"+file.Filename, err)
+			}
+		}
+	})
+}
+
+func upload_multi_file(engine *gin.Engine) {
+	engine.POST("/upload_files", func(ctx *gin.Context) {
+		form, err := ctx.MultipartForm()
+		if err != nil {
+			ctx.String(http.StatusBadRequest, err.Error())
+		} else {
+			files := form.File["files"]
+			for _, file := range files {
+				ctx.SaveUploadedFile(file, "./data/"+file.Filename)
+			}
+			ctx.String(http.StatusOK, "upload"+strconv.Itoa(len(files))+" files")
+		}
+	})
+}
+
+type Student struct {
+	Name     string   `form:"username" json:"name" uri:"user" xml:"user" yaml:"user" binding:"required"`
+	Addr     string   `form:"addr" json:"addr" uri:"addr" xml:"addr" yaml:"addr" binding:"required"`
+	Keywords []string `form:"keywords"`
+}
+
+func formBind(engine *gin.Engine) {
+	engine.POST("/stu/form", func(ctx *gin.Context) {
+		var stu Student
+		if err := ctx.ShouldBind(&stu); err != nil {
+			fmt.Println(err)
+			ctx.String(http.StatusBadRequest, "parse paramter failed")
+		} else {
+			ctx.String(http.StatusOK, stu.Name+" live in "+stu.Addr)
+		}
+	})
+}
+
+func jsonBind(engine *gin.Engine) {
+	engine.POST("/stu/json", func(ctx *gin.Context) {
+		var stu Student
+		if err := ctx.ShouldBindJSON(&stu); err != nil {
+			fmt.Println(err)
+			ctx.String(http.StatusBadRequest, "parse paramter failed")
+		} else {
+			ctx.String(http.StatusOK, stu.Name+" live in "+stu.Addr)
+		}
+	})
+}
+
+func uriBind(engine *gin.Engine) {
+	engine.GET("/stu/uri/:user/*addr", func(ctx *gin.Context) {
+		var stu Student
+		if err := ctx.ShouldBindUri(&stu); err != nil {
+			fmt.Println(err)
+			ctx.String(http.StatusBadRequest, "parse paramter failed")
+		} else {
+			ctx.String(http.StatusOK, stu.Name+" live in "+stu.Addr)
+		}
+	})
+}
+
+func xmlBind(engine *gin.Engine) {
+	engine.POST("/stu/xml", func(ctx *gin.Context) {
+		var stu Student
+		if err := ctx.ShouldBindXML(&stu); err != nil {
+			fmt.Println(err)
+			ctx.String(http.StatusBadRequest, "parse paramter failed")
+		} else {
+			ctx.String(http.StatusOK, stu.Name+" live in "+stu.Addr)
+		}
+	})
+}
+
+func yamlBind(engine *gin.Engine) {
+	engine.POST("/stu/yaml", func(ctx *gin.Context) {
+		var stu Student
+		if err := ctx.ShouldBindYAML(&stu); err != nil {
+			fmt.Println(err)
+			ctx.String(http.StatusBadRequest, "parse paramter failed")
+		} else {
+			ctx.String(http.StatusOK, stu.Name+" live in "+stu.Addr)
+		}
+	})
+}
+
+func multiBind(engine *gin.Engine) {
+	engine.POST("/stu/multi_type", func(ctx *gin.Context) {
+		var stu Student
+		var stu2 idl.Student
+		if err := ctx.ShouldBindBodyWith(&stu, binding.JSON); err == nil {
+			ctx.String(http.StatusOK, stu.Name+" live in "+stu.Addr)
+		} else if err := ctx.ShouldBindBodyWith(&stu, binding.XML); err == nil {
+			ctx.String(http.StatusOK, stu.Name+" live in "+stu.Addr)
+		} else if err := ctx.ShouldBindBodyWith(&stu, binding.YAML); err == nil {
+			ctx.String(http.StatusOK, stu.Name+" live in "+stu.Addr)
+		} else if err := ctx.ShouldBindBodyWith(&stu2, binding.ProtoBuf); err == nil {
+			ctx.String(http.StatusOK, stu2.Name+" live in "+stu2.Address)
+		} else {
+			ctx.String(http.StatusBadRequest, "不支持的参数类型")
+		}
+	})
+}
+
+func main() {
+	engine := gin.Default()
+	url(engine)
+	restful(engine)
+	postForm(engine)
+	postJson(engine)
+	upload_file(engine)       //用postman模拟一个post请求，注意body类型选择form-data，Key名称为file，类型为File，在Value里选择本地文件
+	upload_multi_file(engine) //用postman模拟一个post请求，注意body类型选择form-data，Key名称为files，类型为File，在Value里选择多个本地文件
+
+	formBind(engine)
+	jsonBind(engine)
+	uriBind(engine)
+	xmlBind(engine)
+	yamlBind(engine)
+
+	multiBind(engine)
+
+	engine.Run("127.0.0.1:5678")
 }
